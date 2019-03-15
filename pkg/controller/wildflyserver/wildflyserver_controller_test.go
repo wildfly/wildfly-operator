b3262b6d4710a00b257a81d227624da5b1efbbc7
Jeff Mesnil <jmesnil@gmail.com>
2019-03-15 16:52:41 +0100

    add test for stateful WildFlyServer

--- pkg/controller/wildflyserver/wildflyserver_controller_test.go
+++ pkg/controller/wildflyserver/wildflyserver_controller_test.go
@@ -7,6 +7,7 @@ import (
 	wildflyv1alpha1 "github.com/wildfly/wildfly-operator/pkg/apis/wildfly/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -16,18 +17,18 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
 
+var (
+	name                   = "wildfy-operator"
+	namespace              = "wildfly"
+	replicas         int32 = 3
+	applicationImage       = "my-app-image"
+)
+
 // TestMemcachedController runs ReconcileWildFlyServer.Reconcile() against a
 // fake client that tracks a WildFlyServer object.
-func TestWildFlyServerController(t *testing.T) {
+func TestWildFlyServerControllerCreatesDeployment(t *testing.T) {
 	// Set the logger to development mode for verbose logs.
 	logf.SetLogger(logf.ZapLogger(true))
-
-	var (
-		name                   = "wildfy-operator"
-		namespace              = "wildfly"
-		replicas         int32 = 3
-		applicationImage       = "my-app-image"
-	)
 
 	// A WildFlyServer resource with metadata and spec.
 	wildflyServer := &wildflyv1alpha1.WildFlyServer{
@@ -37,7 +38,8 @@ func TestWildFlyServerController(t *testing.T) {
 		},
 		Spec: wildflyv1alpha1.WildFlyServerSpec{
 			ApplicationImage: applicationImage,
-			Size:             replicas, // Set desired number of Memcached replicas.
+			Size:             replicas,
+			Stateful:         false,
 		},
 	}
 	// Objects to track in the fake client.
@@ -83,5 +85,67 @@ func TestWildFlyServerController(t *testing.T) {
 	dAppImage := dep.Spec.Template.Spec.Containers[0].Image
 	if dAppImage != applicationImage {
 		t.Errorf("applicatiom image (%s) is not the expected image (%s)", dAppImage, applicationImage)
+	}
+}
+
+func TestWildFlyServerControllerCreatesStatefulSet(t *testing.T) {
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(logf.ZapLogger(true))
+
+	// A WildFlyServer resource with metadata and spec.
+	wildflyServer := &wildflyv1alpha1.WildFlyServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: wildflyv1alpha1.WildFlyServerSpec{
+			ApplicationImage: applicationImage,
+			Size:             replicas,
+			Stateful:         true,
+		},
+	}
+	// Objects to track in the fake client.
+	objs := []runtime.Object{
+		wildflyServer,
+	}
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(wildflyv1alpha1.SchemeGroupVersion, wildflyServer)
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	// Create a ReconcileWildFlyServer object with the scheme and fake client.
+	r := &ReconcileWildFlyServer{client: cl, scheme: s}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	// Check the result of reconciliation to make sure it has the desired state.
+	if !res.Requeue {
+		t.Error("reconcile did not requeue request as expected")
+	}
+
+	// Check if stateful set has been created and has the correct size.
+	statefulSet := &appsv1.StatefulSet{}
+	err = cl.Get(context.TODO(), req.NamespacedName, statefulSet)
+	if err != nil {
+		t.Fatalf("get deployment: (%v)", err)
+	}
+	setSize := *statefulSet.Spec.Replicas
+	if setSize != replicas {
+		t.Errorf("dep size (%d) is not the expected size (%d)", setSize, replicas)
+	}
+	setAppImage := statefulSet.Spec.Template.Spec.Containers[0].Image
+	if setAppImage != applicationImage {
+		t.Errorf("applicatiom image (%s) is not the expected image (%s)", setAppImage, applicationImage)
 	}
 }
