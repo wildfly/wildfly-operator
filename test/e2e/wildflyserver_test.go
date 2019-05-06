266ada57de134c65f8644915517f38f0c44c3524
Jeff Mesnil <jmesnil@gmail.com>
2019-05-06 15:10:08 +0200

    [test] clustering e2e test
    Add end-to-end test for clustering that checks in the pods' logs that
    both pods forms a new cluster

--- test/e2e/wildflyserver_test.go
+++ test/e2e/wildflyserver_test.go
@@ -5,8 +5,11 @@ package e2e
 import (
 	goctx "context"
 	"fmt"
+	"io/ioutil"
 	"testing"
 	"time"
+
+	rbac "k8s.io/api/rbac/v1"
 
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
@@ -37,11 +40,12 @@ func TestWildFlyServer(t *testing.T) {
 	}
 	// run subtests
 	t.Run("wildflyserver-group", func(t *testing.T) {
-		t.Run("Cluster", WildFlyCluster)
+		t.Run("WildFlyBasicTest", WildFlyBasicTest)
+		t.Run("WildFlyClusterTest", WildFlyClusterTest)
 	})
 }
 
-func wildflyServerScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+func wildflyBasicServerScaleTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
 	namespace, err := ctx.GetNamespace()
 	if err != nil {
 		return fmt.Errorf("could not get namespace: %v", err)
@@ -70,7 +74,66 @@ func wildflyServerScaleTest(t *testing.T, f *framework.Framework, ctx *framework
 	return wildflyframework.WaitUntilReady(f, t, wildflyServer)
 }
 
-func WildFlyCluster(t *testing.T) {
+func wildflyClusterViewTest(t *testing.T, f *framework.Framework, ctx *framework.TestCtx) error {
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		return fmt.Errorf("could not get namespace: %v", err)
+	}
+
+	name := "clusterbench"
+	standaloneConfigXML, err := ioutil.ReadFile("test/e2e/standalone-clustering-test.xml")
+	if err != nil {
+		return err
+	}
+
+	// create RBAC so that JGroups can view the k8s cluster
+	roleBinding := &rbac.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "RoleBinding",
+			APIVersion: "rbac.authorization.k8s.io",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Subjects: []rbac.Subject{{
+			Kind: "ServiceAccount",
+			Name: "default",
+		}},
+		RoleRef: rbac.RoleRef{
+			Kind:     "ClusterRole",
+			Name:     "view",
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+	}
+	err = f.Client.Create(goctx.TODO(), roleBinding, &framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	if err != nil {
+		return err
+	}
+
+	// create config map for the standalone config
+	wildflyframework.CreateStandaloneConfigMap(f, ctx, namespace, "clusterbench-configmap", "standalone-openshift.xml", standaloneConfigXML)
+	// create wildflyserver custom resource
+	wildflyServer := wildflyframework.MakeBasicWildFlyServer(namespace, name, "quay.io/jmesnil/clusterbench", 2)
+	wildflyServer.Spec.StandaloneConfigMap = &wildflyv1alpha1.StandaloneConfigMapSpec{
+		Name: "clusterbench-configmap",
+		Key:  "standalone-openshift.xml",
+	}
+
+	err = wildflyframework.CreateAndWaitUntilReady(f, ctx, t, wildflyServer)
+	if err != nil {
+		return err
+	}
+
+	err = wildflyframework.WaitUntilReady(f, t, wildflyServer)
+	if err != nil {
+		return err
+	}
+
+	return wildflyframework.WaitUntilClusterIsFormed(f, t, wildflyServer, "clusterbench-0", "clusterbench-1")
+}
+
+func WildFlyBasicTest(t *testing.T) {
 	t.Parallel()
 	ctx := framework.NewTestCtx(t)
 	defer ctx.Cleanup()
@@ -91,7 +154,33 @@ func WildFlyCluster(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if err = wildflyServerScaleTest(t, f, ctx); err != nil {
+	if err = wildflyBasicServerScaleTest(t, f, ctx); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func WildFlyClusterTest(t *testing.T) {
+	t.Parallel()
+	ctx := framework.NewTestCtx(t)
+	defer ctx.Cleanup()
+	err := ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	if err != nil {
+		t.Fatalf("failed to initialize cluster resources: %v", err)
+	}
+	t.Log("Initialized cluster resources")
+	namespace, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// get global framework variables
+	f := framework.Global
+	// wait for wildfly-operator to be ready
+	err = e2eutil.WaitForDeployment(t, f.KubeClient, namespace, "wildfly-operator", 1, retryInterval, timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = wildflyClusterViewTest(t, f, ctx); err != nil {
 		t.Fatal(err)
 	}
 }
