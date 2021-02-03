e3ea8169019a97673576e53ce576ec9a563ce6b2
Ondra Chaloupka <ochaloup@redhat.com>
2021-02-03 18:01:48 +0100

    Fix on sh scripts to not applying removed yaml files where declaration was put under operator.yaml

--- build/run-minikube.sh
+++ build/run-minikube.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f deploy/service_account.yaml
-kubectl apply -f deploy/role.yaml
-kubectl apply -f deploy/role_binding.yaml
 # install WildFlyServer CRD
 kubectl apply -f deploy/crds/wildfly.org_wildflyservers_crd.yaml
-# install WildFly Operator
+# install WildFly Operator (plus service accounts and role binding)
 kubectl create -f deploy/operator.yaml
