4dbde00407ec671b49c41025d4c5886cafa142a9
Yeray Borges <yborgess@redhat.com>
2022-02-21 16:08:00 +0000

    Configure operator-sdk setup

--- build/docker_push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
-docker push quay.io/wildfly/wildfly-operator
--- build/run-openshift.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-oc adm policy add-cluster-role-to-user cluster-admin developer
-oc apply -f deploy/crds/wildfly.org_wildflyservers_crd.yaml
-oc apply -f deploy/operator.yaml
