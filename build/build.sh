1cf9e53ba2d854f45589e4aaa3dfcb89eb176c91
Yeray Borges <yborgess@redhat.com>
2022-02-21 22:17:40 +0000

    Recover build.sh

---
+++ build/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOOS=${1-linux}
+BIN_DIR=${2-$(pwd)/build/_output/bin}
+mkdir -p ${BIN_DIR}
+PROJECT_NAME="wildfly-operator"
+REPO_PATH="github.com/wildfly/wildfly-operator"
+BUILD_PATH="${REPO_PATH}/cmd/manager"
+VERSION="$(git describe --tags --always --dirty)"
+GO_LDFLAGS="-X ${REPO_PATH}/version.Version=${VERSION}"
+echo "building ${PROJECT_NAME}..."
+#For x86_64, the ARCH is amd64
+ARCH=$(uname -m)
+if [[ ${ARCH} == 'x86_64' ]]; then ARCH=amd64; fi
+GOOS=${GOOS} GOARCH=${ARCH} CGO_ENABLED=0 go build -o ${BIN_DIR}/${PROJECT_NAME} -ldflags "${GO_LDFLAGS}" $BUILD_PATH
