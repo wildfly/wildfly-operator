efc92160afd55a1a5ea18ce46105ed18ae2a8077
Yeray Borges <yborgess@redhat.com>
2026-02-02 09:41:36 +0000

    Issue-337 Workaround Docker CLI 2533 issue
    Signed-off-by: Yeray Borges <yborgess@redhat.com>

--- build/build.sh
+++ build/build.sh
@@ -10,4 +10,8 @@ REPO_PATH="github.com/wildfly/wildfly-operator"
 VERSION="$(git describe --tags --always --dirty)"
 GO_LDFLAGS_ARG="-X '${REPO_PATH}/version.Version=${VERSION}'"
 echo "building ${PROJECT_NAME} and tagging with ${IMAGE_NAME}"
+
+# Workaround https://github.com/docker/cli/issues/2533
+export DOCKER_API_VERSION=1.43
+
 docker build -t ${IMAGE_NAME} --build-arg GO_LDFLAGS="${GO_LDFLAGS_ARG}" .
